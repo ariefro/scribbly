b94b0927dccc18e07bd6e60b5c3abe331825ff35
arief romadhon <ariefromadhon26@gmail.com>
2024-12-18 13:51:30 +0700

    add collaborative room component

---
+++ components/CollaborativeRoom.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { ClientSideSuspense, RoomProvider } from "@liveblocks/react";
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
+const CollaborativeRoom = () => {
+  return (
+    <RoomProvider id="my-room">
+      <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <div className="collaborative-room">
+          <Header>
+            <div className="flex w-fit items-center justify-center gap-2">
+              <p className="document-title">This is a dummy document title</p>
+            </div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Header>
+          <Editor />
+        </div>
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
+
+export default CollaborativeRoom;
